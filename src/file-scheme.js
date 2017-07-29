a510c899193fddfb8fab44ab8f2e44c4b8480bba
Markus Felten <Markus.Felten@gmx.de>
2017-07-29 21:55:35 +0200

    feat: add context arg for all url handling methods

--- src/file-scheme.js
+++ src/file-scheme.js
@@ -26,12 +26,13 @@ export default class FileScheme extends URLScheme {
 
   /**
    * Creates a readable stream for the content of th file associated to a given file URL
+   * @param context {Context} execution context
    * @param url {URL} of the a file
    * @param [options] {object|string} passed as options to fs.createReadStream()
    * @returns {Promise}
    * @fulfil {ReadableStream} - of the file content
    */
-  get(url, options) {
+  get(context, url, options) {
     if (url.protocol === 'file:') {
       return Promise.resolve(fs.createReadStream(url.pathname, options));
     }
@@ -41,18 +42,20 @@ export default class FileScheme extends URLScheme {
 
   /**
    * Read stat of a file assiciated to a given file URL
+   * @param context {Context} execution context
    * @param url {URL} of the a file
    * @param [options] {object} unused for now
    * @returns {Promise}
    * @fulfil {object} - as delivered by fs.stat()
    * @reject {Error} - if url is not a file url or fs.stat() error
    */
-  stat(url, options) {
+  stat(context, url, options) {
     return url2file(_stat, url);
   }
 
   /**
    * Put content of a stream to a file associated to a given file URL
+   * @param context {Context} execution context
    * @param url {URL} of the a file
    * @param stream {Stream} data source
    * @param [options] {object|string} passed as options to fs.createWriteStream()
@@ -60,7 +63,7 @@ export default class FileScheme extends URLScheme {
    * @fulfil {undefined} - undefined
    * @reject {Error} - if url is not a file url
    */
-  put(url, stream, options) {
+  put(context, url, stream, options) {
     if (url.protocol === 'file:') {
       return new Promise((fullfill, reject) => {
         stream.pipe(fs.createWriteStream(url.pathname, options));
@@ -73,34 +76,37 @@ export default class FileScheme extends URLScheme {
 
   /**
    * Deletes the file assiciated to a given file URL
+   * @param context {Context} execution context
    * @param url {URL} of the a file
    * @returns {Promise}
    * @fulfil {undefined} - undefined
    * @reject {Error} - as delivered by fs.unlink()
    */
-  delete(url) {
+  delete(context, url) {
     return url2file(_unlink, url);
   }
 
   /**
    * List content of a directory
+   * @param context {Context} execution context
    * @param url {URL} of the a directory
    * @param {object} [options] unused for now
    * @returns {Promise}
    * @fulfil {string[]} - file names
    * @reject {Error} - as delivered by fs.readdir()
    */
-  list(url, options) {
+  list(context, url, options) {
     return url2file(_readdir, url);
   }
 
   /**
    * List content of a directory
+   * @param context {Context} execution context
    * @param url {URL} of the a directory
    * @param {object} [options] unused for now
    * @returns {Iterator}
    */
-  async *_list(url, options) {
+  async *_list(context, url, options) {
     const list = await url2file(_readdir, url);
     for (const entry of list) {
       yield entry;
--- tests/file-test.js
+++ tests/file-test.js
@@ -11,36 +11,42 @@ test('file scheme has name', t => {
 });
 
 test('can get', async t => {
+  const context = undefined;
   const scheme = new FileScheme();
   const aFile = path.join(__dirname, '..', 'tests', 'file-test.js');
-  const content = await scheme.get(new URL('file://' + aFile));
+  const content = await scheme.get(context, new URL('file://' + aFile));
   t.true(content !== undefined);
 });
 
 test('can stat', async t => {
+  const context = undefined;
   const scheme = new FileScheme();
   const aFile = path.join(__dirname, '..', 'tests', 'file-test.js');
-  const stat = await scheme.stat(new URL('file://' + aFile));
+  const stat = await scheme.stat(context, new URL('file://' + aFile));
   t.true(stat.size > 1000 && stat.size < 10000);
 });
 
 test('can put', async t => {
+  const context = undefined;
+
   const scheme = new FileScheme();
   const aFile = path.join(__dirname, 'file2.tmp');
   await scheme.put(
+    context,
     new URL('file://' + aFile),
     fs.createReadStream(path.join(__dirname, '..', 'tests', 'file-test.js'))
   );
-  const stat = await scheme.stat(new URL('file://' + aFile));
+  const stat = await scheme.stat(context, new URL('file://' + aFile));
   t.true(stat.size > 1000 && stat.size < 10000);
 });
 
 test.cb('can delete', t => {
+  const context = undefined;
   const scheme = new FileScheme();
   const aFile = path.join(__dirname, 'file.tmp');
   fs.writeFileSync(aFile, 'someData');
 
-  scheme.delete(new URL('file://' + aFile)).then(() => {
+  scheme.delete(context, new URL('file://' + aFile)).then(() => {
     fs.stat(aFile, error => t.end(error ? undefined : 'not deleted'));
   });
 
@@ -48,17 +54,20 @@ test.cb('can delete', t => {
 });
 
 test('can list', async t => {
+  const context = undefined;
   const scheme = new FileScheme();
   const aDir = path.join(__dirname);
-  const list = await scheme.list(new URL('file://' + aDir));
+  const list = await scheme.list(context, new URL('file://' + aDir));
   t.true(list.includes('test-bundle.js'));
 });
 
 /*
 test('can list async iterator', async t => {
+const context = undefined;
+
   const scheme = new FileScheme();
   const aDir = path.join(__dirname);
-  const list = scheme._list('file://' + aDir);
+  const list = scheme._list(context, 'file://' + aDir);
 
   console.log(list);
 
@@ -70,8 +79,9 @@ test('can list async iterator', async t => {
 
 test('list error', async t => {
   function fn() {
+    const context = undefined;
     const scheme = new FileScheme();
-    return scheme.list(new URL('file:///unknown'));
+    return scheme.list(context, new URL('file:///unknown'));
   }
 
   const error = await t.throws(fn());
