ee8ba28e6bc16b9f17de1fabb51438478b8e479c
Markus Felten <Markus.Felten@gmx.de>
2018-07-19 23:13:06 +0200

    chore(rollup): reformat

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,5 +1,4 @@
 import istanbul from 'rollup-plugin-istanbul';
-
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import multiEntry from 'rollup-plugin-multi-entry';
@@ -25,7 +24,12 @@ export default {
     'https',
     'tty'
   ],
-  plugins: [multiEntry(), resolve(), commonjs(), istanbul({
-    exclude: ['tests/**/*-test.js']
-  })]
+  plugins: [
+    multiEntry(),
+    resolve(),
+    commonjs(),
+    istanbul({
+      exclude: ['tests/**/*-test.js']
+    })
+  ]
 };
